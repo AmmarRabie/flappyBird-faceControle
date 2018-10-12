366fb65fa43d81f63cdaf3195010aff0c8f33073
AmmarRabie <ammaralsayed55@gmail.com>
2018-10-12 03:55:45 +0200

    Use smileDetector class. make the code more organized

--- game.py
+++ game.py
@@ -5,6 +5,7 @@ from os import makedirs
 import cv2
 import sys
 import pygame
+from smiledetector import SmileDetector
 from pygame.locals import K_UP, K_ESCAPE, K_f, K_n
 sys.path.append("game/")
 import wrapped_flappy_bird as game
@@ -13,6 +14,7 @@ import numpy as np
 from collections import deque
 from datetime import datetime
 
+smileDetector = SmileDetector()
 
 ACTIONS = 2 # number of valid actions
 
@@ -24,32 +26,48 @@ ds_factor = 0.5
 
 counter = 0
 APP_CONFIG = {'save':False}
+APP_CONFIG['RECT_WIDTH'] = 150
+APP_CONFIG['RECT_HEIGHT'] = 150
 
-def isMouth():
+def isMouth(faceFrame):
     mouth = False
-    # get the current photo
-    ret, frame = cap.read()
-    frame = cv2.resize(frame, None, fx=ds_factor, fy=ds_factor, interpolation=cv2.INTER_AREA)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(faceFrame, cv2.COLOR_BGR2GRAY)
 
     # inc counter
-    global counter
-    counter = counter + 1
+    #global counter
+    #counter = counter + 1
 
     # save if next image and config is on
-    if (counter % 150 and APP_CONFIG['save']):
-        newImagePath = "{}\\_{}.jpg".format(APP_CONFIG['save_path'],counter)
-        cv2.imwrite(newImagePath, frame)
+    # if (counter % 150 and APP_CONFIG['save']):
+    #     newImagePath = "{}\\_{}.jpg".format(APP_CONFIG['save_path'],counter)
+    #     cv2.imwrite(newImagePath, faceFrame)
 
     # detect smile or fist or ...
     mouth_rects = mouth_cascade.detectMultiScale(gray, 1.7, 11)
     for (x,y,w,h) in mouth_rects:
         y = int(y - 0.15*h)
-        cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 3)
+        # cv2.rectangle(faceFrame, (x,y), (x+w,y+h), (0,255,0), 3)
         mouth = x and y and w and h
-    cv2.imshow('Mouth Detector', frame)
+        break
     return mouth
 
+
+def getGrayFrameFromCamera():
+    # get the current photo
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, None, fx=ds_factor, fy=ds_factor, interpolation=cv2.INTER_AREA)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    return frame
+
+def isMouth2(faceImage):
+    return smileDetector.predict(faceImage)
+    # image = getGrayFrameFromCamera()
+    # if (isMouth):
+    #     cv2.circle(image, (50,50), 10, (0,255,0), 5)
+    # else:
+    #     cv2.circle(image, (50,50), 10, (255,0,0), 5)
+    # cv2.imshow('Mouth Detector', image)
+    # return isMouth
 
 def playGame():
     # open up a game state to communicate with emulator
@@ -68,13 +86,74 @@ def playGame():
             APP_CONFIG['save'] = True
             print('on')
         
-        action = np.zeros(ACTIONS)
-        action[0] = 1
-        if isMouth():
-            action[0] = 0
-            action[1] = 1
-        game_state.frame_step(action)
+        currFrame = getCurrCameraFrame()
+        drawFitRectangle(currFrame)
+        faceFrame = excludeFace(currFrame)
+        shouldJump = isMouth2(faceFrame)
+        game_state.frame_step(getAction(shouldJump))
+        drawFrame(currFrame)
+        saveFrame(currFrame)
 
+
+def getAction(jump):
+    action = np.zeros(ACTIONS)
+    action[0] = 1
+    if jump:
+        action[0] = 0
+        action[1] = 1
+    return action
+
+def drawScreenImage():
+    # get the current photo
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, None, fx=ds_factor, fy=ds_factor, interpolation=cv2.INTER_AREA)
+
+    # get points to draw fit rectangle
+    #height, width = frame.shape
+    #center = (height / 2, width / 2)
+
+    # draw the fit rectangle
+    x1, y1, x2, y2 = calcFitRectangle()
+    cv2.rectangle(frame,(x1,y1), (x2,y2),(0,0,255), 2)
+
+    # draw a status circle and the whole view
+    cv2.circle(frame, (150,0), 20, (255,0,0), 5)
+
+    cv2.imshow('Mouth Detector', frame)
+
+    # crop and return the cropped
+    frame = frame[y1:y2, x1:x2]
+    return frame
+
+
+def calcFitRectangle():
+    x_start, y_start = 75, 50
+    x1, x2, y1, y2 = x_start, x_start + APP_CONFIG['RECT_WIDTH'], y_start, y_start + APP_CONFIG['RECT_HEIGHT']
+    return x1, y1, x2, y2
+
+def drawFitRectangle(frame):
+    x1, y1, x2, y2 = calcFitRectangle()
+    cv2.rectangle(frame,(x1,y1), (x2,y2),(0,0,255), 2)
+
+def excludeFace(frame):
+    x1, y1, x2, y2 = calcFitRectangle()
+    return frame[y1:y2, x1:x2]
+
+def getCurrCameraFrame():
+    return cv2.resize(cap.read()[1], None, fx=ds_factor, fy=ds_factor, interpolation=cv2.INTER_AREA)
+
+def drawFrame(frame):
+    cv2.imshow('Mouth Detector', frame)
+
+def saveFrame(frame):
+    # inc counter
+    global counter
+    counter = counter + 1
+    
+    # save if next image and config is on
+    if (counter % 150 and APP_CONFIG['save']):
+        newImagePath = "{}\\_{}.jpg".format(APP_CONFIG['save_path'],counter)
+        cv2.imwrite(newImagePath, frame)
 def main():
     now = datetime.now()
     h = now.hour
